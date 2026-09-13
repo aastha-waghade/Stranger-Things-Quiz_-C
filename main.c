738d02fa0e5fc1b93188e40523b4f1218c096bec
Aastha Waghade <aasthawaghade@gmail.com>
2026-09-13 23:20:29 +0530

    Added instruction section

--- main.c
+++ main.c
@@ -34,6 +34,7 @@ int main()
         printf("1. You will be asked a series of questions about Stranger Things.\n");
         printf("2. Choose the correct answer from the given options.\n");
         printf("3. Your score will be displayed at the end of the quiz.\n");
+        printf("4. If you want to discontinue then enter exit\n")
         break;
     case 3:
         printf("Exiting the game...\n");
